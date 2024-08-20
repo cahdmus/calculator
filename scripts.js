b85b4dac22ec464757e130aa7ea4321d1abb02d8
cahdmus <bayingpan@gmail.com>
2024-08-20 16:12:02 +0200

    it works ! (I know I'm not great at commit descriptions)

--- scripts.js
+++ scripts.js
@@ -1,75 +1,98 @@
-
 const screen = document.querySelector('#screen');
 const btns = document.querySelectorAll('.btn');
 
 function operate(num1, operator, num2) {
-    if (operator == "+") {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    if (operator === "add") {
         return num1 + num2;
-    } else if (operator == "-") {
+    } else if (operator === "subtract") {
         return num1 - num2;
-    } else if (operator == "*") {
+    } else if (operator === "multiply") {
         return num1 * num2;
-    } else if (operator == "/") {
+    } else if (operator === "divide") {
         return num1 / num2;
     } else {
-        return(num1);
+        return "idk...";
     }
 }
 
-// console.log(operate(1, "*", 3))
+let displayValue;
+let input = [];
+let args = [];
+let result;
 
-function addNum() {
-    
+function getInput() {
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', e => {
+            switch (btn.className) {
+                case 'btn num':
+                    switch (btn.id) {
+                        case 'zero':
+                            input.push(0);
+                            break;
+                        case 'one':
+                            input.push(1);
+                            break;
+                        case 'two':
+                            input.push(2);
+                            break;
+                        case 'three':
+                            input.push(3);
+                            break;
+                        case 'four':
+                            input.push(4);
+                            break;
+                        case 'five':
+                            input.push(5);
+                            break;
+                        case 'six':
+                            input.push(6);
+                            break;
+                        case 'seven':
+                            input.push(7);
+                            break;
+                        case 'eight':
+                            input.push(8);
+                            break;
+                        case 'nine':
+                            input.push(9);
+                            break;
+                        case 'point':
+                            input.push(".");
+                            break;
+                    }
+                    displayValue = input.join("");
+                    screen.textContent = displayValue;
+                    break;
+
+                case 'btn operator':
+                    input = [];
+                    args.push(displayValue);
+                    args.push(btn.id);
+                    displayValue = "---";
+                    screen.textContent = displayValue;
+                    break;
+
+                case 'btn equals':
+                    input = [];
+                    args.push(displayValue);
+                    displayValue = operate.apply(this, args);
+                    screen.textContent = displayValue;
+                    args = [];
+                    break;
+
+                    case 'btn clear':
+                        input = [];
+                        displayValue = "cleared";
+                        screen.textContent = displayValue;
+                        args = [];
+                        break;
+            }
+        })
+    })
 }
 
-let input = [];
-let num1 = 0;
-let displayValue = "0000";
-
-btns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        switch (btn.id) {
-            case 'zero':
-                input.push(0);
-            break;
-            case 'one':
-                input.push(1);
-            break;
-            case 'two':
-                input.push(2);
-            break;
-            case 'three':
-                input.push(3);
-            break;
-            case 'four':
-                input.push(4);
-            break;
-            case 'five':
-                input.push(5);
-            break;
-            case 'six':
-                input.push(6);
-            break;
-            case 'seven':
-                input.push(7);
-            break;
-            case 'eight':
-                input.push(8);
-            break;
-            case 'nine':
-                input.push(9);
-            break;
-            case 'clear':
-                input = [];
-            break;
-            case 'add':
-                num1 = displayValue;
-                input = [];
-
-                console.log(num1);
-            break;
-        }
-        displayValue = input.join("");
-        screen.textContent = displayValue;
-    })
-})
+getInput();
